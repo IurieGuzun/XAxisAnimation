e5b414ae0bf493fb46600824f4a5583b5b1000b5
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-05 12:37:03 -0500

    Initial Commit

---
+++ XAxisAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XAxisAnimation
+//
+//  Created by Iurie Guzun on 2021-01-05.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
